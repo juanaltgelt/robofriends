0cc7e48516580b73f9a4da848450ce0b792bfa91
Juansi93 <93302114+Juansi93@users.noreply.github.com>
2022-02-23 10:22:47 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card.js';
+import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <Card/>
+    <Card/>
+    <Card/>
+  </div>
+  ,
   document.getElementById('root')
 );
 
