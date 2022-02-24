5177b850e90f137ab3710ff704224c3ab62125d5
Juansi93 <93302114+Juansi93@users.noreply.github.com>
2022-02-24 16:57:54 -0300

    updating app

---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import './App.css'
+import CardList from "../components/CardList";
+import Scroll from "../components/Scroll";
+import SearchBox from "../components/SearchBox";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ robots: users}));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(searchfield.toLowerCase());
+    });
+    if (!robots.length) {
+        return <h1 className="tc">Loading...</h1>
+    } else {
+    return (
+      <div className="tc">
+        <h1 className="f2">Robofriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+        <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
+    }
+  }
+}
+
+export default App;
